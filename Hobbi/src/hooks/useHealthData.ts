a0cbea2895957748adeeefefd80be4e8f7299c8d
Gia Fang <giafang16@gmail.com>
2024-02-06 14:45:49 -0800

    Added more data

--- Hobbi/src/hooks/useHealthData.ts
+++ Hobbi/src/hooks/useHealthData.ts
@@ -3,8 +3,11 @@ import AppleHealthKit, {
   HealthInputOptions,
   HealthKitPermissions,
   HealthUnit,
+  HealthValue,
+  AnchoredQueryResults,
 } from "react-native-health";
 import { Platform } from 'react-native';
+import {calculateDuration} from '../utils/dateUtils';
 
 // Starter code for getting permissions and data for AppleHealthKit referenced from https://www.notjust.dev/projects/step-counter/apple-healthkit
 
@@ -14,14 +17,23 @@ const permissions: HealthKitPermissions = {
   permissions: {
     read: [
       Permissions.Steps,
+      Permissions.SleepAnalysis,
+      Permissions.Workout,
     ],
     write: [],
   },
 };
 
 const useHealthData = (date: Date) => {
+  const defaultSleep = {id: 'defaultId', startDate: date, endDate: date, hours: 0, minutes: 0};
+  const yesterday = new Date(date);
+  yesterday.setDate(date.getDate() - 1);
+  // Adjust date to get sleep values from last 24 hours
+
   const [hasPermissions, setHasPermission] = useState(false);
   const [steps, setSteps] = useState(0);
+  const [sleep, setSleep] = useState<SleepLog>({ ...defaultSleep });
+  const [workouts, setWorkouts] = useState<AnchoredQueryResults>();
 
 	useEffect(() => {
     if (Platform.OS !== 'ios') {
@@ -54,20 +66,64 @@ const useHealthData = (date: Date) => {
 
     const options: HealthInputOptions = {
       date: date.toISOString(),
-      includeManuallyAdded: true,
+    };
+
+    const yesterdayOptions: HealthInputOptions = {
+      startDate: yesterday.toISOString(),
     };
 
     AppleHealthKit.getStepCount(options, (err, results) => {
       if (err) {
-        console.log('Error getting the steps');
+        console.log('Error getting steps data');
         return;
       }
       setSteps(results.value);
     });
 
+    AppleHealthKit.getSleepSamples(yesterdayOptions, (err, results) => {
+      if (err) {
+        console.log('Error getting sleep data');
+        return;
+      }
+      // Results are of type HealthValue[]
+      // Must convert to singular HealthValue that represents sleep (gets longest sleep log)
+
+      // console.log(results);
+
+      const longestSleepLog = results.filter((log) => {
+        // HealthValue interface expects value to be a number but makes it a string for some reason...
+        return typeof log.value === 'string' && log.value === 'ASLEEP';
+        }).reduce((prev, current) => {
+        const prevDuration = new Date(prev.endDate).getTime() - new Date(prev.startDate).getTime();
+        const currentDuration = new Date(current.endDate).getTime() - new Date(current.startDate).getTime();
+
+        return prevDuration > currentDuration ? prev : current;
+      });
+
+      const mappedSleepLog: SleepLog = {
+        id: longestSleepLog.id || 'defaultId',
+        startDate: new Date(longestSleepLog.startDate),
+        endDate: new Date(longestSleepLog.endDate),
+        ...calculateDuration(longestSleepLog.startDate, longestSleepLog.endDate)
+      };
+      
+      setSleep(mappedSleepLog);
+    });
+
+    AppleHealthKit.getAnchoredWorkouts(yesterdayOptions, (err, results) => {
+      if (err) {
+        console.log('Error getting workout data');
+        return;
+      }
+      // const allWorkouts 
+      console.log(results.data);
+
+      setWorkouts(results);
+    });
+
   }, [hasPermissions, date]);
   
-  return {steps};
+  return {steps, sleep, workouts};
 };
 
 export default useHealthData;
