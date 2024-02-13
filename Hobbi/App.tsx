31ce0e6a4ef9e36a889bb7726aa87c140934e2ae
Maithy Le <maithyle265@yahoo.com>
2024-02-12 18:59:45 -0800

    Added some icons

--- Hobbi/App.tsx
+++ Hobbi/App.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import Home from "./src/pages/Home";
 import Sleep from "./src/pages/Sleep";
 import Exercise from "./src/pages/Exercise";
@@ -12,8 +13,26 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Exercise" component={Exercise} />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Exercise"
+          component={Exercise}
+          options={{
+            tabBarLabel: "Exercise",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="barbell" color={color} size={size} />
+            ),
+          }}
+        />
         <Tab.Screen name="Sleep" component={Sleep} />
         <Tab.Screen name="Journal" component={Journal} />
       </Tab.Navigator>
